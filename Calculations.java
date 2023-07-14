8d5c37e8b5d4dde02134aaa1fd9663b865c8f7b3
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-14 15:06:08 +0000

    [Add]四則演算、文字列を連結

---
+++ Calculations.java
@@ -0,0 +1,13 @@
+public class Calculations {
+    public static void main(String[] args) {
+        System.out.println("四則演算、文字列を連結");
+        System.out.println(1 + 2);
+        System.out.println(20 - 10);
+        System.out.println(2 * 3);
+        System.out.println(10 / 2);
+        System.out.println(10 % 3);
+        System.out.println("トマト" + "おいしい");
+        // 文字列と数値を + 演算子で連結することが可能。数値は文字列に変換される
+        System.out.println("トマト" + 5 + "個");
+    }
+}
