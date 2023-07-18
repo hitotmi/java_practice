63291bf3bb66c7dbac4113ef6acdc9598ec7a682
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-18 08:44:45 +0000

    [Add]メソッド

---
+++ Method.java
@@ -0,0 +1,19 @@
+public class Method {
+    public static void main(String[] args) {
+        int greeting = 123;
+
+        greetingA();
+
+        System.out.println(greeting);
+    }
+
+    public static void greetingA() {
+        System.out.println("おはようございます！");
+        greetingB
+        ();
+    }
+    
+    public static void greetingB() {
+        System.out.println("こんにちは！");
+    }
+}
