350ba716eb54f71f5c16831096abd860ce17b0e3
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-26 07:03:49 +0000

    [Add]文字列の連結

---
+++ practice3/1-3/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            sb.append("トマト ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
