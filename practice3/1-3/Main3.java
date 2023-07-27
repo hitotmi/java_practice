53f6a0db1110ac562f9326ab47663248d7dbe927
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-27 09:06:41 +0000

    [Add]書式整形

---
+++ practice3/1-3/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+    public static void main(String[] args) {
+        String name = "Tanaka";
+        int age = 30;
+
+        System.out.printf("Name: %s, Age: %d\n", name, age);
+        double pi = 3.141592;
+
+        System.out.printf("Pi: %.2f\n", pi);
+    }
+}
