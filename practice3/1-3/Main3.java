e3fc039fca4ce298c412b887e11452eff6bbbfd4
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-27 10:09:38 +0000

    [Add]日時を扱うクラス

--- practice3/1-3/Main3.java
+++ practice3/1-3/Main3.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main3 {
     public static void main(String[] args) {
         String name = "Tanaka";
@@ -7,5 +10,21 @@ public class Main3 {
         double pi = 3.141592;
 
         System.out.printf("Pi: %.2f\n", pi);
+
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(date);
+
+
+        System.out.println("年は"+calendar.get(calendar.YEAR));
+        //CalendarクラスのMONTHは0から始まるので、実行結果は6月を示します。
+        System.out.println("月は"+calendar.get(calendar.MONTH));
+        System.out.println("月は"+(calendar.get(Calendar.MONTH) + 1));
+        System.out.println("日は"+calendar.get(calendar.DATE));
+        System.out.println("時間は"+calendar.get(calendar.HOUR));
+        System.out.println("分は"+calendar.get(calendar.MINUTE));
+        System.out.println("秒は"+calendar.get(calendar.SECOND));
+        // System.out.println(date);
     }
 }
