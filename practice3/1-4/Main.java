55c932bd71cecd888621a4211fa712f541dfa7b1
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-30 09:36:05 +0000

    [Add]try-with-resources文

--- practice3/1-4/Main.java
+++ practice3/1-4/Main.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("整数を入力してください: ");
             int number = scanner.nextInt();
             System.out.println("入力した数の平方根は: " + Math.sqrt(number));
         } catch (InputMismatchException e) {
             System.out.println("エラー: 整数を入力してください。");
-        } finally {
-            if (scanner != null) {
-                System.out.println("処理終了します");
-                scanner.close();
-            }
         }
     }
 }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = null;
+//         try {
+//             scanner = new Scanner(System.in);
+//             System.out.println("整数を入力してください: ");
+//             int number = scanner.nextInt();
+//             System.out.println("入力した数の平方根は: " + Math.sqrt(number));
+//         } catch (InputMismatchException e) {
+//             System.out.println("エラー: 整数を入力してください。");
+//         } finally {
+//             if (scanner != null) {
+//                 System.out.println("処理終了します");
+//                 scanner.close();
+//             }
+//         }
+//     }
+// }
